cfd6b66c137ef475925b05677c0b09b314ee4c4d
Ray Osborn <rayosborn@mac.com>
2017-11-17 09:54:31 -0600

    Add support for versioneer

--- src/cflib/__init__.py
+++ src/cflib/__init__.py
@@ -1,1 +1,24 @@
-__version__ = '0.1.0'
+__package_name__ = u'CFlib'
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+__documentation_author__ = u'Ray Osborn'
+__documentation_copyright__ = u'2017, Ray Osborn'
+
+__license__ = u'BSD'
+__author_name__ = u'Ray Osborn'
+__author_email__ = u'rayosborn@mac.com'
+__author__ = __author_name__ + u' <' + __author_email__ + u'>'
+
+__description__ = u'CFlib: Python calculator for crystal fields'
+__long_description__ = \
+u"""
+This is a Python package for performing calculations of crystal field (CF) 
+properties of rare earth ions using the Stevens Operator formalism [K. W. H. 
+Stevens, Proc. Phys. Soc. A 65, 209 (1952)]. Once the CF parameters have 
+been initialized, the CF Hamiltonian can be diagonalized to determine the 
+energies and wavefunctions of all the CF levels. These can be used to determine
+the magnetic susceptibility and neutron scattering spectra as a function of 
+temperature.
+"""
